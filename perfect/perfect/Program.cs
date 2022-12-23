45cb049c27bd99d7c5011a79dc39653e1d9ea9b3
amerelfalwo <121310205+amerelfalwo@users.noreply.github.com>
2022-12-23 09:32:51 -0800

    Add files via upload

---
+++ perfect/perfect/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perfect
+{
+    internal class Program
+    {
+        static bool perfect(int number)
+        {
+            int sum = 0;    
+            for(int x=1;x<number;x++) 
+            { 
+                if(number%x==0) sum=sum+x;
+            }
+            if(sum ==number)return true;
+            return false;   
+        }
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.WriteLine("enter the number : ");
+            x= int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the number : ");
+            y= int.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine("\n");
+            for(int i= x; i<=y; i++) 
+            {
+                if (perfect(i)) Console.WriteLine("{0},", i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ prime/prime/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number :");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            for (int x = num1; x <= num2; x++)
+            {
+                bool isprime = true;
+                for (int y = 2; y < x; y++)
+                {
+                    if (x % y == 0)
+                    {
+
+                        isprime = false;
+                        break;
+
+                    }
+
+
+                }
+                if (isprime)
+                {
+                    Console.WriteLine(" is prime : " + x);
+                }
+            }
+        }
+    }
+}
